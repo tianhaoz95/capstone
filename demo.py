c68ac24a39f764c1ccbadf9ad88da261f22c3dcb
tianhaoz95 <tianhaoz@umich.edu>
2018-02-27 01:29:57 -0800

    update

--- demo.py
+++ demo.py
@@ -2,10 +2,8 @@ from capstone.util import *
 
 def main():
     print('starting main ...')
-    # convert_tex_to_html('data/tex_files/faster_rcnn/rpn_pami_arxiv.tex', 'data/html_files/faster_rcnn/output.html')
-    html_file = load_html_file('data/html_files/faster_rcnn/output.html')
-    for it in html_file.find_all(['p', 'math']):
-        print(it.contents)
+    convert_tex_to_html('data/tex_files/faster_rcnn/rpn_pami_arxiv.tex', 'data/xml_files/faster_rcnn/output.xml')
+    # html_file = load_html_file('data/xml_files/faster_rcnn/output.xml')
 
 if __name__ == '__main__':
     main()
