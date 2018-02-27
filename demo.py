e0d6240e627d2c8e71fa12f9910a610b629a2a42
tianhaoz95 <tianhaoz@umich.edu>
2018-02-27 13:18:41 -0800

    update

--- demo.py
+++ demo.py
@@ -2,8 +2,10 @@ from capstone.util import *
 
 def main():
     print('starting main ...')
-    convert_tex_to_html('data/tex_files/faster_rcnn/rpn_pami_arxiv.tex', 'data/xml_files/faster_rcnn/output.xml')
-    # html_file = load_html_file('data/xml_files/faster_rcnn/output.xml')
+    # convert_tex_to_html('data/tex_files/faster_rcnn/rpn_pami_arxiv.tex', 'data/xml_files/faster_rcnn/output.xml')
+    html_file = load_html_file('data/html_files/faster_rcnn/output.html')
+    for seg in html_file.find_all('math'):
+        print(seg['alttext'])
 
 if __name__ == '__main__':
     main()
